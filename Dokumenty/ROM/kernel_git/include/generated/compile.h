d046e870bac3a15b4152873266ccd8b65956cf07
Vangreen <pwangryn@op.pl>
2015-08-17 10:26:31 +0200

    added

---
+++ Dokumenty/ROM/kernel_git/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 6 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#6 SMP PREEMPT Mon Aug 17 10:13:20 CEST 2015"
+#define LINUX_COMPILE_BY "vangreen"
+#define LINUX_COMPILE_HOST "Vangreen"
+#define LINUX_COMPILER "gcc version 4.9.4 20150629 (prerelease) (crosstool-NG 1.20.0 - Linaro GCC 2015.06 - Cortex-A7) "
